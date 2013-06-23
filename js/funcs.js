f5f6bcf96cfe98404793c39ce9664be60bb82122
Saeed Heidari <saeed.heidari86@gmail.com>
2013-06-23 16:38:34 +0430

    0.0.007
    Scrolling done

--- js/funcs.js
+++ js/funcs.js
@@ -17,10 +17,13 @@ function getDayPapers() {
 
 function getDayPapersCallback(data) {
 	$.mobile.hidePageLoadingMsg();
-	console.log(data);
 	$("#home ul.papers").empty();
 	for(var x in data) {
 		$("#home ul.papers").append("<li><img src='"+data[x]["image"]+"' /><h1>"+data[x]["title"]+"</h1></li>");
 	}
+	var count = $("#home ul.papers li").length;
+	var width = count * (parseInt($("#home ul.papers li").width())+24);
+	$("#home .scroller").css({"width":width+"px"});
+	new IScroll('#home .scroller-wrapper', { scrollX: true, scrollY: false, mouseWheel: true });
 	
 }
--- js/sobhaneh.js
+++ js/sobhaneh.js
@@ -1,5 +1,10 @@
+document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
+
 $(document).on('pageshow', '#home', function() {
+
 	$.mobile.showPageLoadingMsg();
-	getDayPapers();	
+	getDayPapers();
 	
-});
+	
+}); 
+
